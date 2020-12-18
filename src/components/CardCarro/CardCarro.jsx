ec5ad276a6c15f0ce7db17c9d2e4fa696f547aec
Fábio Dias <fabbiodiaz@gmail.com>
2020-12-18 14:47:50 -0300

    CardCarro alterado para funcionar como um link para o menu do carro e para salvar os dados do carro em questão no localStorage

--- src/components/CardCarro/CardCarro.jsx
+++ src/components/CardCarro/CardCarro.jsx
@@ -1,8 +1,16 @@
 import './style.css'
+import {useHistory} from 'react-router-dom'
 
 const CardCarro = (props) => {
+    const history = useHistory()
+
+        const clickHandler = () => {
+            localStorage.setItem('apelido',props.apelido)
+            localStorage.setItem('placa',props.placa)
+            history.push('/carro',props)
+        }
     return ( 
-        <div className="card-carro">
+        <div className="card-carro" onClick={() => clickHandler()}>
             <h2 className="apelido">{props.apelido}</h2>
             <p className="placa">Placa: {props.placa}</p>
         </div>
