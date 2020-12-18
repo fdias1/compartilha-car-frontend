957ba48cfd7f9bdbad417e62c9c10663be76d960
Fábio Dias <fabbiodiaz@gmail.com>
2020-12-18 14:46:14 -0300

    Menu alterado para capturar os dados do carro selecioando do localStorage

--- src/components/MenuCarro/MenuCarro.jsx
+++ src/components/MenuCarro/MenuCarro.jsx
@@ -2,21 +2,20 @@ import './style.css'
 import BotaoQuadrado from '../BotaoQuadrado/BotaoQuadrado'
 import Menu from '../Menu/Menu'
 import {useHistory} from 'react-router-dom'
+import {useState} from 'react'
 
 import { faCar,faGasPump,faUsers,faHistory,faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 const MenuCarro = (props) => {
     const history = useHistory()
+    const [state,setState] = useState({apelido:localStorage.getItem('apelido'),placa:localStorage.getItem('placa')})
 
     return ( 
-        <Menu titulo="Nosso Ka com titulo enorme (PWM0261)">
+        <Menu titulo={`${state.apelido} (${state.placa})`}>
             <BotaoQuadrado label="Inserir Kilometragem" id="kilometragem" icon={faCar} onClick={() => history.push('/rodagem')}/>
             <BotaoQuadrado label="Inserir Abastecimento" id="abastecimento" icon={faGasPump} onClick={() => history.push('/abastecer')}/>
             <BotaoQuadrado label="Usuários" id="usuarios" icon={faUsers} onClick={() => history.push('/usuarios')}/>
             <BotaoQuadrado label="Histórico" id="historico" icon={faHistory} onClick={() => history.push('/historico')}/>
-            {
-            //<BotaoQuadrado label="Outras Funções" id="outras" icon={faEllipsisH}/>
-            }
             <BotaoQuadrado labelOnly label="Voltar" id="voltar" icon={faArrowLeft} onClick={() => history.push('/home')}/>
         </Menu>
      );
